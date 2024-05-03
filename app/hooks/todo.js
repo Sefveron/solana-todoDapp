cc680d855be125920c6d5891cda5f630b23fcf27
sefve <sefve@hotmail.com>
2024-05-03 16:38:05 +0200

    Add netlify 9

--- app/hooks/todo.js
+++ app/hooks/todo.js
@@ -51,7 +51,7 @@ let dummyTodos = [
     {
         account:{
             idx: '5',
-            content: 'Read english book chapter 15',
+            content: 'Read english book chapter 16',
             marked: true,          
         }
     },
